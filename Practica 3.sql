311f74af8c11988d859392ba734915ddfc815f72
AngelTorresBv <54971993+AngelTorresBv@users.noreply.github.com>
2020-03-09 13:17:47 -0600

    Add files via upload

---
+++ Practica 3.sql
@@ -0,0 +1,270 @@
+USE [Hospital]
+GO
+/****** Object:  Index [Doctores_Enfermeras]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP INDEX IF EXISTS [Doctores_Enfermeras] ON [dbo].[Urgencias]
+GO
+/****** Object:  Index [UC_Urgencias_id]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Urgencias]') AND type in (N'U'))
+ALTER TABLE [dbo].[Urgencias] DROP CONSTRAINT IF EXISTS [UC_Urgencias_id]
+GO
+/****** Object:  Index [UC_Salas_id]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Salas]') AND type in (N'U'))
+ALTER TABLE [dbo].[Salas] DROP CONSTRAINT IF EXISTS [UC_Salas_id]
+GO
+/****** Object:  Index [INDX_Paciente_NombreCompleto]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP INDEX IF EXISTS [INDX_Paciente_NombreCompleto] ON [dbo].[Pacientes]
+GO
+/****** Object:  Index [UC_paciente_id]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Pacientes]') AND type in (N'U'))
+ALTER TABLE [dbo].[Pacientes] DROP CONSTRAINT IF EXISTS [UC_paciente_id]
+GO
+/****** Object:  Index [UC_Centros_id]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CentrosDeControl]') AND type in (N'U'))
+ALTER TABLE [dbo].[CentrosDeControl] DROP CONSTRAINT IF EXISTS [UC_Centros_id]
+GO
+/****** Object:  Index [UC_Cama_id]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Camas]') AND type in (N'U'))
+ALTER TABLE [dbo].[Camas] DROP CONSTRAINT IF EXISTS [UC_Cama_id]
+GO
+/****** Object:  Table [dbo].[Urgencias]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP TABLE IF EXISTS [dbo].[Urgencias]
+GO
+/****** Object:  Table [dbo].[Salas]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP TABLE IF EXISTS [dbo].[Salas]
+GO
+/****** Object:  Table [dbo].[Pacientes]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP TABLE IF EXISTS [dbo].[Pacientes]
+GO
+/****** Object:  Table [dbo].[CentrosDeControl]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP TABLE IF EXISTS [dbo].[CentrosDeControl]
+GO
+/****** Object:  Table [dbo].[Camas]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP TABLE IF EXISTS [dbo].[Camas]
+GO
+USE [master]
+GO
+/****** Object:  Database [Hospital]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+DROP DATABASE IF EXISTS [Hospital]
+GO
+/****** Object:  Database [Hospital]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+CREATE DATABASE [Hospital]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Hospital', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\Hospital.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Hospital_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\Hospital_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+ALTER DATABASE [Hospital] SET COMPATIBILITY_LEVEL = 140
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Hospital].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [Hospital] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [Hospital] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [Hospital] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [Hospital] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [Hospital] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [Hospital] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [Hospital] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [Hospital] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [Hospital] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [Hospital] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [Hospital] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [Hospital] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [Hospital] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [Hospital] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [Hospital] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [Hospital] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [Hospital] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [Hospital] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [Hospital] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [Hospital] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [Hospital] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [Hospital] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [Hospital] SET RECOVERY FULL 
+GO
+ALTER DATABASE [Hospital] SET  MULTI_USER 
+GO
+ALTER DATABASE [Hospital] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [Hospital] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [Hospital] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [Hospital] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [Hospital] SET DELAYED_DURABILITY = DISABLED 
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'Hospital', N'ON'
+GO
+ALTER DATABASE [Hospital] SET QUERY_STORE = OFF
+GO
+USE [Hospital]
+GO
+/****** Object:  Table [dbo].[Camas]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Camas](
+	[Cama_id] [int] NOT NULL,
+	[NPiso] [varchar](50) NOT NULL,
+	[Activo] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Cama_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CentrosDeControl]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CentrosDeControl](
+	[Centros_id] [int] NOT NULL,
+	[SalasActivas] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Centros_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Pacientes]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Pacientes](
+	[paciente_id] [int] NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[PrimerApellido] [varchar](50) NOT NULL,
+	[SegundoApellido] [varchar](50) NOT NULL,
+	[NombreCompleto]  AS (((([Nombre]+' ')+[PrimerApellido])+' ')+[SegundoApellido]),
+	[FechaDeNacimiento] [datetime] NOT NULL,
+	[Edad]  AS (datediff(year,[FechaDeNacimiento],getdate())),
+PRIMARY KEY CLUSTERED 
+(
+	[paciente_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Salas]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Salas](
+	[Salas_id] [int] NOT NULL,
+	[NSala] [varchar](50) NOT NULL,
+	[NPiso] [varchar](50) NOT NULL,
+	[Activo] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Salas_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Urgencias]    Script Date: 09/03/2020 01:14:51 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Urgencias](
+	[Urgencias_id] [int] NOT NULL,
+	[Doctor] [varchar](50) NOT NULL,
+	[Enfermera] [varchar](50) NOT NULL,
+	[Paciente] [varchar](50) NOT NULL,
+	[PersonasEnSala]  AS (((([Doctor]+' ')+[Enfermera])+' ')+[Paciente]),
+PRIMARY KEY CLUSTERED 
+(
+	[Urgencias_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Index [UC_Cama_id]    Script Date: 09/03/2020 01:14:52 p. m. ******/
+ALTER TABLE [dbo].[Camas] ADD  CONSTRAINT [UC_Cama_id] UNIQUE NONCLUSTERED 
+(
+	[Cama_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+/****** Object:  Index [UC_Centros_id]    Script Date: 09/03/2020 01:14:52 p. m. ******/
+ALTER TABLE [dbo].[CentrosDeControl] ADD  CONSTRAINT [UC_Centros_id] UNIQUE NONCLUSTERED 
+(
+	[Centros_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+/****** Object:  Index [UC_paciente_id]    Script Date: 09/03/2020 01:14:52 p. m. ******/
+ALTER TABLE [dbo].[Pacientes] ADD  CONSTRAINT [UC_paciente_id] UNIQUE NONCLUSTERED 
+(
+	[paciente_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ARITHABORT ON
+SET CONCAT_NULL_YIELDS_NULL ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+SET NUMERIC_ROUNDABORT OFF
+GO
+/****** Object:  Index [INDX_Paciente_NombreCompleto]    Script Date: 09/03/2020 01:14:52 p. m. ******/
+CREATE NONCLUSTERED INDEX [INDX_Paciente_NombreCompleto] ON [dbo].[Pacientes]
+(
+	[NombreCompleto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+/****** Object:  Index [UC_Salas_id]    Script Date: 09/03/2020 01:14:52 p. m. ******/
+ALTER TABLE [dbo].[Salas] ADD  CONSTRAINT [UC_Salas_id] UNIQUE NONCLUSTERED 
+(
+	[Salas_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+/****** Object:  Index [UC_Urgencias_id]    Script Date: 09/03/2020 01:14:52 p. m. ******/
+ALTER TABLE [dbo].[Urgencias] ADD  CONSTRAINT [UC_Urgencias_id] UNIQUE NONCLUSTERED 
+(
+	[Urgencias_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+SET ARITHABORT ON
+SET CONCAT_NULL_YIELDS_NULL ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+SET NUMERIC_ROUNDABORT OFF
+GO
+/****** Object:  Index [Doctores_Enfermeras]    Script Date: 09/03/2020 01:14:52 p. m. ******/
+CREATE NONCLUSTERED INDEX [Doctores_Enfermeras] ON [dbo].[Urgencias]
+(
+	[PersonasEnSala] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [Hospital] SET  READ_WRITE 
+GO
